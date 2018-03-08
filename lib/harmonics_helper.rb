e4bbd36ddb73e6ab1d06b8ab8543a5ee23a2cbcc
RomanceChika <romance.yamada@gmail.com>
2018-03-09 01:15:11 +0900

    first commit gem style

---
+++ lib/harmonics_helper.rb
@@ -0,0 +1,5 @@
+require "harmonics_helper/version"
+
+module HarmonicsHelper
+  # Your code goes here...
+end
---
+++ spec/harmonics_helper_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe HarmonicsHelper do
+  it "has a version number" do
+    expect(HarmonicsHelper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
