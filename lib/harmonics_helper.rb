4c4abe8c0391f9ff51e5779d556c69477a141a3c
RomanceChika <romance.yamada@gmail.com>
2018-03-13 00:35:03 +0900

    [bugfix] remove invalid require

--- lib/harmonics_helper.rb
+++ lib/harmonics_helper.rb
@@ -4,7 +4,6 @@ require "harmonics_helper/xml_parser"
 require "harmonics_helper/single_part"
 require "harmonics_helper/pair_parts"
 
-require "harmonics_helper/code/all_parts"
 require "harmonics_helper/code/rotations"
 
 require "harmonics_helper/etc/exceptions"
