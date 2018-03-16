9ef904cf3c5be8975d8988aaa33970b2f9b14a2e
RomanceChika <romance.yamada@gmail.com>
2018-03-17 01:43:29 +0900

    [implement] output beta

--- lib/harmonics_helper.rb
+++ lib/harmonics_helper.rb
@@ -5,6 +5,8 @@ require "harmonics_helper/single_part"
 require "harmonics_helper/pair_parts"
 require "harmonics_helper/prohibit_checker"
 
+require "harmonics_helper/output_formatter"
+
 require "harmonics_helper/code/rotations"
 
 require "harmonics_helper/etc/exceptions"
---
+++ lib/harmonics_helper/output_formatter.rb
@@ -0,0 +1,25 @@
+require "harmonics_helper/prohibit_checker"
+
+module HarmonicsHelper
+
+  class OutputFormatter
+
+    def initialize(prohibit_checker, file_name="../../../result.txt")
+      @prohibit_checker = prohibit_checker
+      @file_name = file_name
+    end
+
+    # output 
+    # TODO: output should be formatted
+    def output
+      puts "conencutivet octave or fifth"
+      puts "true is probably prohibit"
+      puts @prohibit_checker.concencutive_prohibits_all.each{ |prohibits| }.flatten.any?
+      @prohibit_checker.concencutive_prohibits_all.each{ |prohibits| p prohibits }
+      puts "is code?"
+      puts "false is not code or has not 3rd tone"
+      p @prohibit_checker.code_configured_all.all?
+      p @prohibit_checker.code_configured_all
+    end
+  end
+end
