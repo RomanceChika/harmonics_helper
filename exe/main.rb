993b4ab7306e41c6093ce890def81ad66437670e
RomanceChika <romance.yamada@gmail.com>
2018-03-23 01:03:53 +0900

    [modify] return hash is able to convert json format

--- exe/main.rb
+++ exe/main.rb
@@ -1,8 +1,11 @@
 require "harmonics_helper"
+require "json"
 
 def file_path(file_name)
   File.join(File.dirname(__FILE__), '../files/' + file_name)
 end
 xml = open(file_path("GoodSample.xml"))
 formatter = HarmonicsHelper::ProhibitFormatter.new(xml)
-p formatter.prohibit_info
+result_hash = formatter.result_hash
+
+puts result_hash["prohibit_info"]
